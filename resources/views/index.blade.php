af3e9e9a7410c3a1ed33b3ca610f9851442bf386
gkennedy87 <gkennedy87@gmail.com>
2021-01-09 20:05:13 +0900

    added authentication to BookController

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,13 @@
 @extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="jumbotron">
+            <h1 class="display-4">Welcome to {{config('app.name')}}</h1>
+            <p class="lead">A Laravel-driven Library Management App</p>
+            <hr class="my-4">
+            <p>Sign up today! It's completely free!</p>
+            <a class="btn btn-primary btn-lg" href="{{ route('register') }}">{{ __('Sign Up') }}</a>
+        </div>
+    </div>    
+@endsection
